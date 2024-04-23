8e501a04a22d7dec212c47112356a8745e77eed7
tianxiaoyan2024 <161792631+tianxiaoyan2024@users.noreply.github.com>
2024-04-23 12:40:45 +0800

    Update r_database.R

--- r_database.R
+++ r_database.R
@@ -12,6 +12,7 @@
 
 cat("\014") #clears rhe console
 rm(list=ls()) #remove all variales# Load required libraries
+
 library(reticulate)
 library(DBI)
 
@@ -22,25 +23,44 @@ library(DBI)
 # install.packages("RSQLite")
 # install.packages("RPostgreSQL")
 
+# Load the Doubs dataset from the ade4 package
+library(ade4)
+data("doubs")
+
 # For SQLite
-# library(RSQLite)
-# db_conn <- dbConnect(SQLite(), dbname = "mydatabase.sqlite")
+library(RSQLite)
+db_conn <- dbConnect(SQLite(), dbname = "mydatabase.sqlite")
+
+dbWriteTable(db_conn, 'fish', doubs$fish)
+dbWriteTable(db_conn, 'species', doubs$species)
+dbWriteTable(db_conn, 'env', doubs$env)
+dbWriteTable(db_conn, 'xy', doubs$xy)
+
+# List the tables
+dbListTables(db_conn)
+
+# Close the database connection
+dbDisconnect(db_conn)
 
 # For PostgreSQL
 library(RPostgreSQL)
-db_conn <- dbConnect(PostgreSQL(), 
+library(RPostgres)
+db_conn <- dbConnect(RPostgres::Postgres(), 
                      host = "localhost", 
                      port = 5432, 
                      user = "xytian", 
                      password = "****", 
-                     dbname = "postgres")
+                     dbname = "test_doubs")
+
+# Check the connection
+dbIsValid(db_conn)
 
 # Load the Doubs dataset from the ade4 package
 library(ade4)
-data("Doubs")
+data("doubs")
 
 # Convert the Doubs dataset to a data frame
-doubs_df <- as.data.frame(Doubs)
+doubs_df <- as.data.frame(doubs)
 
 # Upload the data frame to the database
 dbWriteTable(db_conn, "doubs_table", doubs_df, overwrite = TRUE)
